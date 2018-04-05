8c925440fb6def65b92ff8ae5ff3a5619eb62e03
atran123 <atran333@gmail.com>
2018-04-05 16:01:19 -0400

    Add files via upload

--- sequence1a/js/main.js
+++ sequence1a/js/main.js
@@ -4,30 +4,32 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, cr
 // load resources
 
 function preload() {
-    game.load.spritesheet('cards', 'assets/cards.gif', 81, 117.4);
-    game.load.image('bg', 'assets/table.jpg');
+    game.load.spritesheet('chicken', 'assets/chicken.png', 37.5, 37.5);
+    game.load.image('bg', 'assets/farm.jpg');
     game.load.audio('completeMusic', ['assets/Level_Clear.mp3']);
     game.load.audio('gameoverMusic', ['assets/Game_Over.mp3']);
-    game.load.audio('gameMusic', ['assets/casino.mp3']);
+    game.load.audio('gameMusic', ['assets/River Valley Breakdown.mp3']);
+    game.load.audio('squawk', ['assets/killChicken.mp3']);
 }
 
 //----------------------------------------------------------------------------------------
 // global variables
 
+var targets;
 var background;
 var fireButton = null;
 
-var cursors;
-var ship;
-
+var score = 0;
+var scoreText;
+var chickenCount = 60;
+var chickenCountText;
+var text;
+var loop;
 var music;
 var lost = false;
 var won = false;
-
-var score1, score2, score3, score4, score5, score6;
-var compScore, playerScore;
-
-var random1, random2, random3, random4, random5, random6;
+var time = 60000;
+var timer;
 
 function quitGame() {
     //  Here you should destroy anything you no longer need.
@@ -38,6 +40,15 @@ function quitGame() {
 
 function create() {
 
+	// Create a custom timer
+    timer = game.time.create();
+    
+	//  Set a TimerEvent to occur after 30 seconds
+    timer.loop(time, endTimer, this);
+
+	// Start the timer
+    timer.start();
+
     // Add music
     music = game.add.audio('gameMusic');
     music.loop = true;
@@ -45,216 +56,156 @@ function create() {
     
     gameover = game.add.audio('gameoverMusic');
     complete = game.add.audio('completeMusic');
-    
-    //------------------------------------------------------------------------------------
-    // Random frames for the cards
-    random1 = Math.floor(Math.random() * 52);
-    random2 = Math.floor(Math.random() * 52);
-    random3 = Math.floor(Math.random() * 52);
-    random4 = Math.floor(Math.random() * 52);
-    random5 = Math.floor(Math.random() * 52);
-    random6 = Math.floor(Math.random() * 52);
-    
-    //------------------------------------------------------------------------------------
-    //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    //------------------------------------------------------------------------------------
-    //  Resize our game world to be a 2000 x 2000 square
-    //game.world.setBounds(-1000, -1000, 2048, 2048);
-
-    //  A simple background for our game
-    background = game.add.sprite( 0, 0, 'bg' );
-    //background = game.add.tileSprite(0, 0, 800, 600, 'bg');
-
-    //------------------------------------------------------------------------------------
-   	// Create player card sprites
-    card1 = game.add.sprite(110, game.world.height-84, 'cards',random1);
-    card2 = game.add.sprite(200, game.world.height-84, 'cards',random2);
-    card3 = game.add.sprite(290, game.world.height-84, 'cards',random3);
-    
-    // Create computer card sprites
-    card4 = game.add.sprite(110, 84, 'cards', random4);
-    card5 = game.add.sprite(200, 84, 'cards', random5);
-    card6 = game.add.sprite(290, 84, 'cards', random6);
-	
-    // Anchor the sprites at their center, as opposed to its top-left corner.
-    // so it will be truly centered.
-    card1.anchor.setTo( 0.5, 0.5 );
-    card2.anchor.setTo( 0.5, 0.5 );
-    card3.anchor.setTo( 0.5, 0.5 );
-    
-    card4.anchor.setTo( 0.5, 0.5 );
-    card5.anchor.setTo( 0.5, 0.5 );
-    card6.anchor.setTo( 0.5, 0.5 );
-    
-    //------------------------------------------------------------------------------------
-    //  The scores texts
-    //compScoreText = game.add.text(400, 20, "COMPUTER'S SCORE: 0", { fontSize: '30px', fill: '#f40' });
-    compScoreText = game.add.text(400, 20, "COMPUTER'S SCORE: 0", { fontSize: '30px', fill: '#ff0' });
-    playerScoreText = game.add.text(400, game.world.height-60, "YOUR SCORE: 0", { fontSize: '30px', fill: '#f40' });
-	//------------------------------------------------------------------------------------
-	
-    //  The scores
-	gameText = game.add.text( game.world.centerX-200, game.world.height-340, "", { fontSize: '24px', fill: '#00FFFF', align: "center" } );
-
-	//------------------------------------------------------------------------------------
-    //  And some controls to play the game with
-    cursors = game.input.keyboard.createCursorKeys();
-    fireButton = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+    squawk = game.add.audio('squawk');
     
     //------------------------------------------------------------------------------------
     
     restartButton = game.input.keyboard.addKey(Phaser.KeyCode.ENTER);
     
     //------------------------------------------------------------------------------------
-    //  The scores
-    if ((random1 >=0) && (random1 <=8))
-    	score1 = random1 +2;
-    else if ((random1 >=13) && (random1 <=21))
-    	score1 = random1 -11;
-    else if ((random1 >=26) && (random1 <=34))
-    	score1 = random1 -24;
-    else if ((random1 >=39) && (random1 <=47))
-    	score1 = random1 -37;
-    else if ((random1 == 9) || (random1 == 10) || (random1 == 11)
-    		|| (random1 == 22) || (random1 == 23) || (random1 == 24)
-    		|| (random1 == 35) || (random1 == 36) || (random1 == 37)
-    		|| (random1 == 48) || (random1 == 49) || (random1 == 50))
-    	score1 = 10;
-    else if ((random1 == 12) || (random1 == 25) || (random1 == 38) || (random1 == 51))
-    	score1 = 1;
-    	
-    if ((random2 >=0) && (random2 <=8))
-    	score2 = random2 +2;
-    else if ((random2 >=13) && (random2 <=21))
-    	score2 = random2 -11;
-    else if ((random2 >=26) && (random2 <=34))
-    	score2 = random2 -24;
-    else if ((random2 >=39) && (random2 <=47))
-    	score2 = random2 -37;
-    else if ((random2 == 9) || (random2 == 10) || (random2 == 11)
-    		|| (random2 == 22) || (random2 == 23) || (random2 == 24)
-    		|| (random2 == 35) || (random2 == 36) || (random2 == 37)
-    		|| (random2 == 48) || (random2 == 49) || (random2 == 50))
-    	score2 = 10;
-    else if ((random2 == 12) || (random2 == 25) || (random2 == 38) || (random2 == 51))
-    	score2 = 1;
-    	
-    if ((random3 >=0) && (random3 <=8))
-    	score3 = random3 +2;
-    else if ((random3 >=13) && (random3 <=21))
-    	score3 = random3 -11;
-    else if ((random3 >=26) && (random3 <=34))
-    	score3 = random3 -24;
-    else if ((random3 >=39) && (random3 <=47))
-    	score3 = random3 -37;
-    else if ((random3 == 9) || (random3 == 10) || (random3 == 11)
-    		|| (random3 == 22) || (random3 == 23) || (random3 == 24)
-    		|| (random3 == 35) || (random3 == 36) || (random3 == 37)
-    		|| (random3 == 48) || (random3 == 49) || (random3 == 50))
-    	score3 = 10;
-    else if ((random3 == 12) || (random3 == 25) || (random3 == 38) || (random3 == 51))
-    	score3 = 1;
-    	
-    if ((random4 >=0) && (random4 <=8))
-        score4 = random4 +2;
-    else if ((random4 >=13) && (random4 <=21))
-        score4 = random4 -11;
-    else if ((random4 >=26) && (random4 <=34))
-        score4 = random4 -24;
-    else if ((random4 >=39) && (random4 <=47))
-        score4 = random4 -37;
-    else if ((random4 == 9) || (random4 == 10) || (random4 == 11)
-            || (random4 == 22) || (random4 == 23) || (random4 == 24)
-            || (random4 == 35) || (random4 == 36) || (random4 == 37)
-            || (random4 == 48) || (random4 == 49) || (random4 == 50))
-        score4 = 10;
-    else if ((random4 == 12) || (random4 == 25) || (random4 == 38) || (random4 == 51))
-        score4 = 1;
-        
-    if ((random5 >=0) && (random5 <=8))
-        score5 = random5 +2;
-    else if ((random5 >=13) && (random5 <=21))
-        score5 = random5 -11;
-    else if ((random5 >=26) && (random5 <=34))
-        score5 = random5 -24;
-    else if ((random5 >=39) && (random5 <=47))
-        score5 = random5 -37;
-    else if ((random5 == 9) || (random5 == 10) || (random5 == 11)
-            || (random5 == 22) || (random5 == 23) || (random5 == 24)
-            || (random5 == 35) || (random5 == 36) || (random5 == 37)
-            || (random5 == 48) || (random5 == 49) || (random5 == 50))
-        score5 = 10;
-    else if ((random5 == 12) || (random5 == 25) || (random5 == 38) || (random5 == 51))
-        score5 = 1;
-        
-    if ((random6 >=0) && (random6 <=8))
-        score6 = random6 +2;
-    else if ((random6 >=13) && (random6 <=21))
-        score6 = random6 -11;
-    else if ((random6 >=26) && (random6 <=34))
-        score6 = random6 -24;
-    else if ((random6 >=39) && (random6 <=47))
-        score6 = random6 -37;
-    else if ((random6 == 9) || (random6 == 10) || (random6 == 11)
-            || (random6 == 22) || (random6 == 23) || (random6 == 24)
-            || (random6 == 35) || (random6 == 36) || (random6 == 37)
-            || (random6 == 48) || (random6 == 49) || (random6 == 50))
-        score6 = 10;
-    else if ((random6 == 12) || (random6 == 25) || (random6 == 38) || (random6 == 51))
-        score6 = 1;
-    
+
+    //  We're going to be using physics, so enable the Arcade Physics system
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    //------------------------------------------------------------------------------------
+
+    //  A simple background for our game
+    background = game.add.sprite( 0, 0, 'bg' );
+    //background = game.add.tileSprite(0, 0, 800, 600, 'bg');
+
+    //------------------------------------------------------------------------------------
+
+    targets = game.add.group();
+    targets.physicsBodyType = Phaser.Physics.ARCADE;
+    targets.enableBody = true;
+    //  Enables all kind of input actions on this image group (click, etc)
+    targets.inputEnableChildren = true;
+
+    //------------------------------------------------------------------------------------
+
+    // create targets on the screen, and repeat at 1 secs interval for 59 times
+    //  The first parameter is how long to wait before the event fires. In this case 1 seconds (you could pass in 2000 as the value as well.)
+    //  The second parameter is how many times the event will run in total. Here we'll run it 59 times.
+    //  The next two parameters are the function to call ('createTargets') and the context under which that will happen.
+    //  Once the event has been called 59 times it will never be called again.
+    createTargets();
+    loop = game.time.events.repeat(Phaser.Timer.SECOND * 1, 59, createTargets, this);
+
+    //------------------------------------------------------------------------------------
+
+    //  The score
+    scoreText = game.add.text(60, 8, 'SCORE: 0', { fontSize: '40px', fill: '#f40' });
+    chickenCountText = game.add.text(360, 8, 'CHICKENS LEFT: 60', { fontSize: '40px', fill: '#0000FF' });
+    text = game.add.text( game.world.centerX-360, game.world.height-40, "SHOOT 20/60 CHICKENS WITH MOUSE CLICK OR TOUCH.", { fontSize: '24px', fill: '#00FFFF', align: "center" } );
 }
 
 //----------------------------------------------------------------------------------------
-
-//function update() {}
 
 function update() {
-	//  Add and update the score
-	compScore = (score4 + score5 + score6)%10;
-	compScoreText.text = "COMPUTER'S SCORE: " + compScore;
-	
-	playerScore = (score1 + score2 + score3)%10;
-	playerScoreText.text = "YOUR SCORE: " + playerScore;
-	
-	if (compScore < playerScore)
-    {
-        //reset score, change text
-        gameText.text = "YOU WON! Press ENTER to restart";
-        gameText.visible = true;
-        won = true;
 
-        //stop music
-        //music.stop();
-        //complete.play();
-    }
-    else if (compScore > playerScore)
-    {
-        //reset score, change text
-        gameText.text = "YOU LOST! Press ENTER to restart";
-        gameText.visible = true;
-        won = true;
+    //update the chicken count text
+    chickenCountText.text = 'CHICKENS LEFT: ' + chickenCount;
 
-        //stop music
-        //music.stop();
-        //complete.play();
-    } 
-    else 
-    {
-        //reset score, change text
-        gameText.text = "TIE! Press ENTER to restart";
-        gameText.visible = true;
-        won = true;
-
-        //stop music
-        //music.stop();
-        //complete.play();
+    if (lost === true){
+        //the "press ENTER to restart" handler
+        if (restartButton.isDown) {resetScores();music.stop();gameover.stop();game.state.restart();}
     }
     
-    //the "click to restart" handler
-    game.input.onTap.addOnce(function() { resetScores();music.stop();game.state.restart();}, this);
+    if (won === true){
+        //the "press ENTER to restart" handler
+        if (restartButton.isDown) {resetScores();complete.stop();game.state.restart();}
+    }
+    
 }
 
 //----------------------------------------------------------------------------------------
+
+//create targets
+function createTargets(){
+
+    var randomLR = Math.round(Math.random());
+    var randomY = Math.round(Math.random()) + Math.round(Math.random());
+    
+    if (randomLR === 0){ // target appears from the left
+    	// Create a sprite at the left edge of the screen using the 'chicken' image.
+		target = targets.create((Math.random()/2)*game.world.width,randomY*128+128, 'chicken');	
+		// Turn on the arcade physics engine for this sprite.
+		game.physics.arcade.enable(target);
+		// move target sprite horizontally across the screen
+    	target.body.velocity.setTo((Math.random() + 1) * 150, -(Math.random() + 1) * 150);
+    	//  Our 3 animations, flying left, right, and up.
+		target.animations.add('up', [0, 1, 2, 3], 10, true);
+   		target.animations.add('right', [4, 5, 6, 7], 10, true);
+    	target.animations.add('left', [8, 9, 10, 11], 10, true);
+    	// play animation
+    	target.animations.play('right');
+    }
+    else if (randomLR === 1){ // target appears from the right
+    	// Create a sprite at the left edge of the screen using the 'chicken' image.
+		target = targets.create((Math.random()/2+0.5)*game.world.width,randomY*128+128, 'chicken');	
+		// Turn on the arcade physics engine for this sprite.
+		game.physics.arcade.enable(target);
+        //target.body.velocity.x = - (Math.random() + 1) * 150;
+        target.body.velocity.setTo(-(Math.random() + 1) * 150, -(Math.random() + 1) * 150);
+        //  Our 3 animations, flying left, right, and up.
+        target.animations.add('up', [0, 1, 2, 3], 10, true);
+   		target.animations.add('right', [4, 5, 6, 7], 10, true);
+    	target.animations.add('left', [8, 9, 10, 11], 10, true);
+    	// play animation
+    	target.animations.play('left');
+    }
+    
+    // listen for input events
+    targets.onChildInputDown.add(listener, this);
+    
+    //update the chicken count
+    chickenCount -= 1;
+    
+    text = game.add.text(250, 75, '', { fill: '#000000' });
+
+}
+
+function listener (sprite) {
+
+	//  Add and update the score
+	score += 0.5;
+	scoreText.text = 'SCORE: ' + score;
+	
+	squawk.play();
+	sprite.kill();
+
+    if (score === 20)
+    {
+        //reset score, stop creating targets, change text
+        game.time.events.remove(loop);
+        text.text = " YOU WON! Press ENTER to restart";
+        text.visible = true;
+        won = true;
+
+        //stop music
+        music.stop();
+        complete.play();
+    }
+}
+
+function resetScores(){
+    score=0;
+    chickenCount=60;
+}
+
+function endTimer() {
+	// Stop the timer when the delayed event triggers
+	timer.stop();
+	//music.stop();
+	//if ((score < 20) && (chickenCount == 0))
+	if (score < 20)
+	{
+        //reset score, stop creating targets, change text
+        game.time.events.remove(loop);
+        text.text = " YOU LOST! Press ENTER to restart";
+        text.visible = true;
+        music.stop();
+        gameover.play();
+        lost = true;
+    }
+}
